27cc1ff347e6108d1203edffb4f5300646db9140
bsdayo <bsdayo@outlook.com>
2023-12-07 11:37:04 +0800

    feat: initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Dapper;
+using Oracle.ManagedDataAccess.Client;
+
+[module: DapperAot]
+
+await using var connection = new OracleConnection(
+    "Data Source=localhost:1521/service;User Id=my_user;Password=my_password");
+
+var xn = "xn arg";
+var xqm = "xqm arg";
+
+var result = await connection.QueryAsync<string>(
+    "SELECT DISTINCT MY_ROW FROM MY_VIEW WHERE XN = :arg AND XQM = :xqm",
+    new { xn, xqm });
